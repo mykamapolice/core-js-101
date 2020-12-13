184c2f47d589690a22c63db43d11f92ed829c7c4
Ilya Barachenia <ilya.barachenia@gmail.com>
2020-12-13 19:49:42 +0100

    Update 09-functions-n-closures-tasks.js

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -24,7 +24,7 @@
  *
  */
 function getComposition(f, g) {
-  return function (x) {
+  return function a(x) {
     return f(g(x));
   };
 }
@@ -47,7 +47,7 @@ function getComposition(f, g) {
  *
  */
 function getPowerFunction(exponent) {
-  return function (x) {
+  return function a(x) {
     return x ** exponent;
   };
 }
